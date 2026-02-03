3ff1aa528e3bf8f250d7d11323d930a2f013fddc
FreePlacki <jk216635@gmail.com>
2026-02-03 16:50:08 +0100

    Fix provider not being alive long enough

--- lib/login/auth_controller.dart
+++ lib/login/auth_controller.dart
@@ -9,7 +9,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'auth_controller.g.dart';
 
-@riverpod
+@Riverpod(keepAlive: true)
 class AuthController extends _$AuthController {
   @override
   Future<void> build() async {}
